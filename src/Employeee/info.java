4ab2a04b2b4b78054197cf989e16772c8a952695
ramprasathmk <mkramprasath12@gmail.com>
2024-05-02 00:34:01 +0545

    fix: info

--- src/Employeee/info.java
+++ src/Employeee/info.java
@@ -4,16 +4,16 @@ public class info {
     // About the Developer Team
 
     @SuppressWarnings("unused")
-    private static void ramprasathInfo() {
+	public static String viewInfo() {
 
         final String Name = "RAMPRASATH M K";
         final String ramprasathmk = "RAMPRASATH M K";
         final String github = "@ramprasathmk";
 
-        System.out.println("info.ramprasathInfo()");
+        return "info.ramprasathInfo()";
     }
 
     public static void main(String[] args) {
-        ramprasathInfo();
+    	info.viewInfo();
     }
 }
