2facb96fccdd02e68c0a5ad807c19deb6dac6da4
ramprasathmk <mkramprasath12@gmail.com>
2024-05-01 21:33:20 +0545

    fix: info.java

--- src/Employeee/info.java
+++ src/Employeee/info.java
@@ -1,6 +1,19 @@
 package Employeee;
 
 public class info {
-    final static String author = "RAMPRASATH M K";
-    final static String github = "@ramprasathmk";
+    // About the Developer Team
+
+    @SuppressWarnings("unused")
+    private static void ramprasathInfo() {
+
+        final String Name = "RAMPRASATH M K";
+        final String ramprasathmk = "RAMPRASATH M K";
+        final String github = "@ramprasathmk";
+
+        System.out.println("info.ramprasathInfo()");
+    }
+
+    public static void main(String[] args) {
+        ramprasathInfo();
+    }
 }
