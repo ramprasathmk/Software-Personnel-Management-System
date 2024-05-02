7a2811b8c52ec1d78baf8580751cdbfdfd2ec5b3
ramprasathmk <mkramprasath12@gmail.com>
2024-05-02 14:28:18 +0545

    Update testApp

--- src/test/testAPP.java
+++ src/test/testAPP.java
@@ -1,31 +1,25 @@
 package test;
-
 import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.Test;
 import Employeee.*;
-
 public class testAPP {
 
 	@Test
 	public void test() {
 		assertEquals(1, 1);
 	}
-	
 	@Test
 	public void test0() {
 		String res = info.viewInfo();
 		assertEquals( "info.ramprasathInfo()", res);
 	}
-	
 	@Test
 	public void test1() {
 		assertNotNull(new Home());
 	}
-	
 	@Test
 	public void test2() {
 		assertNotNull(new Login().isDisplayable());
 	}
-
 }
