d80fc4293cca063a485a873a2797ccad0093aaf1
ramprasathmk <mkramprasath12@gmail.com>
2024-05-02 14:26:29 +0545

    feat: Update testApp

--- test/testApp.java
+++ test/testApp.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import Employeee.*;
 
-class testApp {
+public class testApp {
 	@Test
 	public void test() {
 		assertEquals(1, 1);
@@ -13,15 +13,20 @@ class testApp {
 	public void test0() {
 		String res = info.viewInfo();
 		assertEquals( "info.ramprasathInfo()", res);
+		System.out.println("Passed Test Case: 1");
 	}
 	
 	@Test
 	public void test1() {
 		assertNotNull(new Home());
+		System.out.println("Passed Test Case: 1");
+
 	}
 	
 	@Test
 	public void test2() {
 		assertNotNull(new Login().isDisplayable());
+		System.out.println("Passed Test Case: 1");
+
 	}
 }
